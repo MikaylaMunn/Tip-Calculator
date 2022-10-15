4dc7cbe8c25dc2b5b178a1fae209d193d459e82b
Mikayla Munn <mikayla.munn@gmail.com>
2022-10-15 09:56:19 -0400

    Made some adjustments

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,42 +1,46 @@
 # Make a python script  tip_calculator.py that takes a user's input at the command line for:
 print("Welcome to Mikayla's Tip Calculator")
-# Prompt the user to enter the amount on the bill includes the sales tax
-# Add a try/except block here
-def validate_user_input():
+
+
+def checking_user_input_for_num():
     is_total = False
+    # Add a try/except block here because what if the user doesn't enter in a number
     while not is_total:
-        overall_total_bill = input("What is the total bill?: ")
+        # Prompt the user to enter the amount on the bill includes the sales tax
+        total_bill_from_user = input("What is the total bill? (Example: 20 for $20.00): ")
         try:
-            overall_total_bill = float(overall_total_bill)
+            total_bill_from_user = float(total_bill_from_user)
             # Assume there is a 10% sales tax. Don't forget to add this into the total bill!
-            sale_tax = overall_total_bill * 0.10
-            overall_total_bill = overall_total_bill + sale_tax
+            sale_tax = total_bill_from_user * 0.10
+            total_bill_from_user = total_bill_from_user + sale_tax
             # Prompt the user for the % tip they'd like to leave
             tip_percentage = int(
                 input(
-                    "What percentage tip would you like to give? (Example: 5, 10,15, 20): "
+                    "What percentage tip would you like to give? (Example: 5, 10, 15, 20): "
                 )
             )
             if tip_percentage < 10:
                 print(
-                    "Seriously, you should rethink this! Or better yet leave right now, remove yourself from my section!"
+                    "Seriously, you should rethink this! Or better yet leave right now, remove yourself from my section!\n"
                 )
             elif tip_percentage >= 10 or tip_percentage <= 20:
-                print("That was nice of you!")
+                print("That was nice of you! Please come see me again! \n")
             else:
-                print("WOW, CAN WE DO THIS AGAIN!")
+                print("WOW, CAN WE DO THIS AGAIN! I APPREICATE IT \n")
             # Prompt the user for the # of people splitting the bill
             num_of_people = int(input("How many people are splitting the bill?: "))
             print("\n")
+            # What if no one wants to claim their bill 
             if num_of_people == 0:
-                print("Yo, seriously a dine and dash.. You're childish!")
+                print("Yo, a dine and dash.. You're childish!")
                 num_of_people = int(
                     input("Seriously, how many people are splitting the bill?: ")
                 )
+            # What if there is a single person and no one to split it
             elif num_of_people == 1:
                 amount_to_pay_per_person = "%.2f" % round(
                     float(
-                        ((tip_percentage / 100 + 1) * overall_total_bill)
+                        ((tip_percentage / 100 + 1) * total_bill_from_user)
                         / num_of_people
                     ),
                     2,
@@ -46,17 +50,19 @@ def validate_user_input():
             else:
                 amount_to_pay_per_person = "%.2f" % round(
                     float(
-                        ((tip_percentage / 100 + 1) * overall_total_bill)
+                        ((tip_percentage / 100 + 1) * total_bill_from_user)
                         / num_of_people
                     ),
                     2,
                 )
             # Print the $ amount of the tip
-            tip_amount = "%.2f" % float(tip_percentage / 100 * overall_total_bill)
+            tip_amount = "%.2f" % float(tip_percentage / 100 * total_bill_from_user)
             # convert total bill and tip amount to floats so they can be added
-            overall_total_bill_float = round(float(overall_total_bill), 2)
+            total_bill_from_user_float = round(float(total_bill_from_user), 2)
             tip_amount_float = round(float(tip_amount), 2)
-            tip_and_total = "%.2f" % float(overall_total_bill_float + tip_amount_float)
+            tip_and_total = "%.2f" % float(
+                total_bill_from_user_float + tip_amount_float
+            )
             # The total bill (including tip)
             print(
                 f"Tip amount: ${tip_amount}\nTotal bill including tip: ${tip_and_total}\nEach person owes: ${amount_to_pay_per_person}"
@@ -67,6 +73,4 @@ def validate_user_input():
             print(
                 "You did not enter a valid number, seriously you need to pay your bill."
             )
-
-
-validate_user_input()
+checking_user_input_for_num()
